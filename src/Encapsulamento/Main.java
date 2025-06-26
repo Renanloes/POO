b15b2603c18c17d1b818a300438e53a44cabfaf1
Renan Loes <renanloes8@gmail.com>
2025-06-26 17:29:16 -0300

    Getter e setters

--- src/Encapsulamento/Main.java
+++ src/Encapsulamento/Main.java
@@ -5,10 +5,15 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
 
         String titular = "Renan";
         String numeroConta = null;
         Double valor = 0.0;
+        
+        int botao = 0;
+        String mensagem = "O que você deseja fazer " + titular + "\n\n" + "1 - Depositar\n" + "2 - Sacar\n" + "3 - Saldo atual\n" + "4 - Sair\n";
 
         contaBancaria conta = new contaBancaria();
 
@@ -17,11 +22,34 @@ public class Main {
 
         System.out.println("Hello, everyone, that is " + conta.getTitular());
         
-        depositar(valor);
-    }
-
-    public static void depositar(Double valor){
-        conta.getSaldo(saldo);
         
+        while(botao != 4){
+            
+            System.out.println(mensagem);
+            botao = input.nextInt();
+            
+            switch(botao){
+                case 1:
+                    Depositar();
+                    break;
+                
+                case 2:
+                    Sacar();
+                    break;
+                    
+                case 5:
+                    conta.getsaldo(valor);
+                    break;
+                    
+                case 4:
+                    System.out.println("Até a próxima " + titular);
+                    break;
+                    
+                default:
+                    System.out.println("Escolha uma das opções existentes");
+                    botao = 0;
+                    break;
+            }
+        }
     }
 }
